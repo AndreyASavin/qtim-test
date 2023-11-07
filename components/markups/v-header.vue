d80ecd6a09f0dc94e66778714819f6f9753e7f17
Andrey <savin.andrey199@gmail.com>
2023-11-07 19:52:53 +0300

    feat: add basic markup for header

---
+++ components/markups/v-header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="section">
+    <div class="header">
+      <div class="logo">ЛОГОТИП</div>
+      <div class="navigation">
+        <div class="navigation__menu">
+          <div class="navigation__link">Works</div>
+          <div class="navigation__link">About</div>
+        </div>
+        <div class="navigation__buttons">
+          <div class="navigation__button">Flag</div>
+          <div class="navigation__button">Let's work</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <vHeader/>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import vHeader from '~/components/markups/v-header.vue';
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
