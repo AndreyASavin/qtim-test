7d44a1a36a61b51ac77bdf03321801b47d9dabfb
Andrey <savin.andrey199@gmail.com>
2023-11-08 12:30:48 +0300

    feat: add styles

--- components/markups/v-footer.vue
+++ components/markups/v-footer.vue
@@ -1,41 +1,41 @@
 <template>
-  <div class="section">
+  <div class="section violet bottom">
     <div class="container">
-      <div class="row">
+      <div class="row t-9">
         <div class="col-12">
           Is there a project?
         </div>
       </div>
-      <div class="row">
+      <div class="row t-7">
         <div class="col-8">Let’s work together</div>
-        <div class="col-4">
+        <div class="col-2">
           <p>For all questions</p>
-          <p>+7 (920) 278—00—68</p>
-          <p>qtim@agency.com</p>
-          <p>Office in Russia</p>
-          <p>139 Ruben Station Lake Drew, NM 52678</p>
-          <p>TGLogo</p>
-          <p>Office in Dubai</p>
-          <p>139 Ruben Station Lake Drew, NM 52678</p>
-          <p>TGLogo</p>
+          <p class="t-2">+7 (920) 278—00—68</p>
+          <p class="t-0">qtim@agency.com</p>
+          <p class="t-5">Office in Russia</p>
+          <p class="t-2">139 Ruben Station Lake Drew, NM 52678</p>
+          <p class="t-1">TGLogo</p>
+          <p class="t-5">Office in Dubai</p>
+          <p class="t-2">139 Ruben Station Lake Drew, NM 52678</p>
+          <p class="t-1">TGLogo</p>
         </div>
       </div>
-      <footer class="footer">
-        <div class="footer__links">
-          <div class="footer__link">Facebook</div>
-          <div class="footer__link">Instagram</div>
-          <div class="footer__link">Telegram</div>
-        </div>
-        <div class="footer__button">
-          Go
-        </div>
-        <div class="footer__links">
-          <div class="footer__link">Behance</div>
-          <div class="footer__link">Linkedin</div>
-          <div class="footer__link">Privacy&nbsp;Policy</div>
-        </div>
-      </footer>
     </div>
+    <footer class="footer t-9">
+      <div class="footer__links">
+        <div class="footer__link">Facebook</div>
+        <div class="footer__link">Instagram</div>
+        <div class="footer__link">Telegram</div>
+      </div>
+      <div class="footer__button">
+        Go
+      </div>
+      <div class="footer__links">
+        <div class="footer__link">Behance</div>
+        <div class="footer__link">Linkedin</div>
+        <div class="footer__link">Privacy&nbsp;Policy</div>
+      </div>
+    </footer>
   </div>
 </template>
 
