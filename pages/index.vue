0d3cabbe76778ad6aa0597474823052d10c17baf
Andrey <savin.andrey199@gmail.com>
2023-11-08 18:57:00 +0300

    feat: add pagination

--- pages/index.vue
+++ pages/index.vue
@@ -9,12 +9,25 @@
       <div class="row b-8">
         <div class="cards-container">
           <div class="card-container"
-          v-for="item in articles" :key="item.id">
+          v-for="item in articlesAtPage" :key="item.id">
             <v-card
             :image="item.image"
             :description="item.description"
             >
             </v-card>
+            {{item.id}}
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <div class="pages">
+            <div
+              class="page"
+              v-for="page in pages" :key="page"
+              @click="setActivePage(Number(page))"
+              :class="{'page-active': page===data.activePage}"
+              >{{page}}</div>
           </div>
         </div>
       </div>
@@ -26,8 +39,20 @@
 import { useArticles } from '~~/store/articles.js';
 const store = useArticles();
 const { fetchArticles } = store;
-const { articles } = storeToRefs(store);
+const { articles, pages } = storeToRefs(store);
 await fetchArticles();
+const data = reactive({
+  activePage: 1,
+  onePageCount: 8,
+});
+function setActivePage(value: number) {
+  data.activePage = value;
+}
+const articlesAtPage = computed(() => {
+  return articles.value.slice(
+    data.onePageCount * (data.activePage - 1), 
+    data.onePageCount * (data.activePage - 1) + 8)
+})
 </script>
 
 <style lang="scss" scoped>
--- store/articles.ts
+++ store/articles.ts
@@ -3,17 +3,18 @@ import { defineStore } from "pinia";
 export const useArticles = defineStore('articles', {
   state: () => ({
     articles: [],
+    pages: 0
   }),
   actions: {
     async fetchArticles() {
       try {
         const { data }: any = await useFetch('https://6082e3545dbd2c001757abf5.mockapi.io/qtim-test-work/posts');
         if (data.value) {
-          this.articles = data.value
+          this.articles = data.value;
+          this.pages = Math.ceil(data.value.length / 8);
         }
       } catch (error) {
         console.log(error);
-        
       }
     },
   },
