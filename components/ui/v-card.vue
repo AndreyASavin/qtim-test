ab9b272069e2efcada535f895fdb90cc599c6b6b
Andrey <savin.andrey199@gmail.com>
2023-11-08 13:28:45 +0300

    feat: add catd component with basic markup

---
+++ components/ui/v-card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <div class="card__content">
+      <div class="card__image">
+        <img src="/images/image.png" alt="">
+      </div>
+      <div class="card__description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga totam quis explicabo sit!
+        </p>
+      </div>
+    </div>
+    <div class="card__link">
+      <p>Read more</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,40 @@
 <template>
-  <div>
-    <h1>Articles</h1>
+  <div class="section">
+    <div class="container">
+      <div class="row t-8">
+        <div class="col-12">
+          <h1>Articles</h1>
+        </div>
+      </div>
+      <div class="row cards-container">
+        <div class="col-3">
+          <v-card></v-card>
+        </div>
+        <div class="col-3">
+          <v-card></v-card>
+        </div>
+        <div class="col-3">
+          <v-card></v-card>
+        </div>
+        <div class="col-3">
+          <v-card></v-card>
+        </div>
+      </div>
+      <div class="row t-6 b-8">
+        <div class="col-3">
+          <v-card></v-card>
+        </div>
+        <div class="col-3">
+          <v-card></v-card>
+        </div>
+        <div class="col-3">
+          <v-card></v-card>
+        </div>
+        <div class="col-3">
+          <v-card></v-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
